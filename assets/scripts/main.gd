0ad7707dc07e4cd507f18c5c102a0fff981292d1
Albert Stefanov <aastefanov@outlook.com>
2017-09-11 10:39:36 +0300

    Basic menu

--- assets/scripts/main.gd
+++ assets/scripts/main.gd
@@ -4,25 +4,26 @@ export (PackedScene) var platform = null
 var spawnTime = 0;
 
 func _ready():
-	
+	platformSpawner()
 	set_fixed_process(true)
 	#spawnPlatform(Vector2(get_viewport().get_rect().size.x,get_viewport().get_rect().size.y/2))
 	
 func _fixed_process(delta):
-	if (spawnTime >= 3):
-		 platformSpawner()
-		 spawnTime = 0
+	#if (spawnTime >= 3):
+	#	 platformSpawner()
+	#	 spawnTime = 0
 	if (Input.is_action_pressed("exit")):
 		get_tree().quit()
-	spawnTime += delta;
+	#spawnTime += delta;
 
 
 func spawnPlatform(var pos):
 	var spawnedPlatform = platform.instance()
 	spawnedPlatform.set_pos(pos)
 	add_child(spawnedPlatform)
+
 	
 func platformSpawner():
-	spawnPlatform(Vector2(get_viewport().get_rect().size.x,get_viewport().get_rect().size.y/2))
+	spawnPlatform(Vector2(get_viewport().get_rect().size.x, get_viewport().get_rect().size.y/2))
 
 	
