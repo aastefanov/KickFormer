f54192823f1ce5b06712a1c2d49b9ef5b665e518
Nikolay Rangelov <NikiRann@gmail.com>
2017-09-23 11:47:33 +0300

    Added very simple shooting

--- assets/scripts/player/player.gd
+++ assets/scripts/player/player.gd
@@ -21,10 +21,32 @@ onready var anim = get_node("animation")
 onready var rightRaycast = get_node("Right")
 onready var leftRaycast = get_node("Left")
 onready var database = get_parent().get_node("Database")
+onready var leftShot = get_node("leftShot")
+onready var rightShot = get_node("rightShot")
+onready var testLabel = get_parent().get_node("Test")
 
+export (PackedScene) var bulletScene = null
 
+func shoot():
+	var shootingPoint
+	if (direction):
+		shootingPoint = leftShot.get_global_pos()
+	else:
+		shootingPoint = rightShot.get_global_pos()
+	var bullet = bulletScene.instance()
+	var shootAngle = findAngle(get_pos(),get_global_mouse_pos())
+	shootAngle -= 90
+	if shootAngle < 0:
+		shootAngle = 360 + shootAngle
+		
+	#print(cos(90), sin(deg2rad(180)))
+	bullet.shoot(shootingPoint,shootAngle)
+	get_parent().add_child(bullet)
 
 func _fixed_process(delta):
+	#testLabel.set_text(str(findAngle(get_pos(),get_global_mouse_pos())) + "            " + str(rad2deg(get_pos().angle_to_point(get_global_mouse_pos()))))
+	if (Input.is_action_pressed("shoot")):
+		shoot()
 	movement(delta)
 
 
@@ -60,7 +82,6 @@ func movement(var delta):
 				if (get_global_pos().x < groundcheck.get_collider().get_global_pos().x):
 					
 					collisionpoint = rightRaycast.get_collision_point().x
-					#print (collisionpoint)
 				else:
 					collisionpoint = leftRaycast.get_collision_point().x
 				var percent = ((collisionpoint - startpos)/lenght) * 100
@@ -88,11 +109,15 @@ func movement(var delta):
 func modify_jump_value(var percent):
 	var jump_start_id = database.get_stat_by_name("jump_start")
 	var player_id = get_parent().player_id 
-	var jump_start_row = database.get_player_stat(jump_start_id, player_id);
+	var jump_start_row = database.get_player_stat(jump_start_id, player_id)
 	var insert_value = (jump_start_row["value"] * jump_start_row["weigth"] + percent) / (jump_start_row["weigth"] + 1)
 	database.set_value_to_stat(jump_start_id, insert_value, player_id)
-	#print(database.get_player_stat(jump_start_id, player_id))
 
+func findAngle(var point1, var point2):
+	var theta = atan2(point2.x - point1.x, point1.y - point2.y)
+	if theta < 0:
+		theta += 2*PI
+	return rad2deg(theta)
 
 func _ready():
 	jumpcast.add_exception(self)
