2f6f5e56e76521870fe0949f854791b136aa7d4c
Nikolay Rangelov <NikiRann@gmail.com>
2017-09-24 17:13:37 +0300

    Added resolution ratio

--- assets/scripts/globals.gd
+++ assets/scripts/globals.gd
@@ -1,6 +1,12 @@
 extends Node
 
 var currentLevel = 0
+var ratio = 1
+export (int) var desiredWidth = 1920
+export (int) var desiredHeight = 1080
 
 func _ready():
-	pass
+	var currentScreenSize = get_tree().get_root().get_rect().size
+	var desiredSize = Vector2(desiredWidth, desiredHeight)
+	ratio = currentScreenSize / desiredSize
+	
--- assets/scripts/main.gd
+++ assets/scripts/main.gd
@@ -1,31 +1,29 @@
 extends Node2D
 
-export (PackedScene) var platform = null
+#export (PackedScene) var platform = null
 export (PackedScene) var levelScene = null
-var spawnTime = 0;
-const spawncount = 1
+var spawnTime = 0
+#const spawncount = 1
 onready var database = get_node("Database")
 var player_id
 
 func _ready():
+	print(get_node("/root/globals").ratio)
 	randomize()
 	database.register("Nikolay")
 	database.seed_stats()
 	player_id = database.get_player_by_name("Nikolay")
 	database.set_high_score(0, player_id)
-	get_node("music").play("hard")
+	#get_node("music").play("hard")
 	set_fixed_process(true)
 	
 	
 func _fixed_process(delta):
 
 	get_node("CanvasLayer/Score").set_text(str(get_node("/root/globals").currentLevel + 1))
-	if (spawnTime >= 2):
-		 #platformSpawner()
-		 spawnTime = 0
-	if (Input.is_action_pressed("exit")):
+	if Input.is_action_pressed("exit") :
 		get_tree().quit()
-	spawnTime += delta;
+	
 
 func generateNextLevel():
 	var level = levelScene.instance()
